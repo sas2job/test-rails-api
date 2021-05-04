c2bd0d5809c8811672f60afee59e596baef46e54
Alex Smirnov <sas2job@gmail.com>
2021-05-04 21:44:29 +0300

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness:  true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
