81ce86e4c15327cf8cf106a821fd054ea4b7fd22
Alex Smirnov <sas2job@gmail.com>
2021-05-07 12:20:16 +0300

    Add frozen string

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,26 +1,32 @@
-class Api::V1::UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
+# frozen_string_literal: true
 
-  def show
-    @user = User.find(params[:id])
-    render json: @user
-  end
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.all
+        render json: @users
+      end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user
-    else
-      render error: { error: 'Unable to create User.' }
+      def show
+        @user = User.find(params[:id])
+        render json: @user
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user
+        else
+          render error: { error: 'Unable to create User.' }
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
+      end
     end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
   end
 end
