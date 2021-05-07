91a034080acb748ca58b43452314b5d9ec0983d5
Alex Smirnov <sas2job@gmail.com>
2021-05-07 10:22:37 +0300

    Add create action for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,18 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render error: { error: 'Unable to create User.' }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
