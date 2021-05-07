197b5b90ea54c0cdcec54163261cd0a107653665
Alex Smirnov <sas2job@gmail.com>
2021-05-07 10:21:50 +0300

    Add show action for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
 end
