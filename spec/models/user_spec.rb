adae0b33e7ba74d12389d004ffb3344bc7fe5208
Alex Smirnov <sas2job@gmail.com>
2021-05-08 15:09:30 +0300

    Add test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'creation' do
-    it 'can be created' do
-      user = User.create(name: 'testuser', email: 'test@example.com', password: 'Password!',
-                         password_confirmation: 'Password!')
+  before(:all) do
+    @user1 = create(:user)
+  end
 
-      expect(user).to be_valid
-    end
+  it 'is valid with valid attributes' do
+    expect(@user1).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(build(:user, name: nil)).to_not be_valid
   end
 end
