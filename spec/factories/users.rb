a3530b7e63d5d82ffc582fe2e150ce6dc262722f
Alex Smirnov <sas2job@gmail.com>
2021-05-08 15:08:39 +0300

    Add factory for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "factoryuser#{n}" }
+    sequence(:email) { |n| "factoryuser#{n}@example.com" }
+    password { 'Password1!' }
+    password_confirmation { 'Password1!' }
   end
 end
