6e9ff738a020ca442e5a874ac8a3dffe94e07951
Alex Smirnov <sas2job@gmail.com>
2021-05-09 12:42:40 +0300

    Password's value in range 6..72

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness:  true
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { in: 6..72 }, on: :create
 end
