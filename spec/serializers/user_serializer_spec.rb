98a9e587b001617831f42e63c0d224a1bcb63b25
Alex Smirnov <sas2job@gmail.com>
2021-05-29 12:26:35 +0300

    Change rspec test

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UserSerializer, type: :serializer do
 	describe 'as_json' do
-		it 'includes name and email' do
+		it "returns the current user" do
 			spec_user = FactoryBot.build( :user, name: 'specuser', email: 'specuser@email.com' )
 			serialized = JSON.parse(spec_user.serializable_hash.to_json)
-			expect( serialized[ "name" ] ).to eq "specuser"
-      expect( serialized[ "email" ] ).to eq "specuser@email.com"
-		end
+      expect(serialized).to match_response_schema('user')
+    end
 	end
 end
